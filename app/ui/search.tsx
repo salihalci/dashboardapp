6f26dd1511a5283b2010cdcd254eb9a168247e3a
salci <salih.alci@gmail.com>
2024-10-19 11:42:28 +0300

    pagination added.

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -16,6 +16,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
 
 
         const params = new URLSearchParams(searchParams);
+
+        params.set('page', '1');
+
         if (term) {
             params.set('query', term);
         } else {
